448744b2ffc41347050336c5158f0c4cb9ff21d6
Rafael <rafael.olivsou@gmail.com>
2025-12-02 22:36:12 -0300

    Initial Commit

---
+++ FinanceApp/FinanceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinanceAppApp.swift
+//  FinanceApp
+//
+//  Created by Rafael Souza Dutra on 02/12/25.
+//
+
+import SwiftUI
+
+@main
+struct FinanceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FinanceAppTests/FinanceAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  FinanceAppTests.swift
+//  FinanceAppTests
+//
+//  Created by Rafael Souza Dutra on 02/12/25.
+//
+
+import Testing
+@testable import FinanceApp
+
+struct FinanceAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
